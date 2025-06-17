c744149cc45ad47fab480534d948c90f8770e11c
Paulo <paulovitor88@gmail.com>
2025-06-17 09:54:20 -0300

    Develop (#3)
    * change: Atualiza gitignore
    Adiciona diretório venv do Python ao .gitignore
    
    * change: Atualiza dockerignore
    
    * change: Atualiza gitignore
    
    * change: Atualiza dockerignore
    
    * fix: Adiciona tratamento de erros e modularização do código
    
    * fix: Adiciona melhorias relacionada a API_KEY
    
    * change: Altera prompt do Agente
    
    Altera prompr do agente para qualificar como especialista em análise de dados
    
    * add: Adiciona funcionalidade registra histórico de execução
    
    ---------
    
    Co-authored-by: jpierre06 <jpierre06@gmail.com>
    Co-authored-by: Jean Santana <57160649+jpierre06@users.noreply.github.com>
    Co-authored-by: paulosantos-dock <129757252+paulosantos-dock@users.noreply.github.com>

---
+++ core/logger.py
@@ -0,0 +1,23 @@
+import os
+import json
+from datetime import datetime
+
+def save_chat_log(question: str, answer: str, code: str, save_dir: str = "./logs"):
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+    
+    filename = f"historico_chat_{timestamp}.json"
+    filepath = os.path.join(save_dir, filename)
+
+    os.makedirs(save_dir, exist_ok=True)
+
+    log_data = {
+        timestamp: [
+            {"pergunta": question},
+            {"resposta": answer},
+            {"codigo_python": code}
+        ]
+    }
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        json.dump(log_data, f, ensure_ascii=False, indent=4)
