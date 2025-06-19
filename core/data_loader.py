cf8d0a73dc64fc16fcb5d624726a6af473e14d04
jpierre06 <jpierre06@gmail.com>
2025-06-19 12:18:00 -0300

    change: Normaliza nome das colunas dos dataframes

--- core/data_loader.py
+++ core/data_loader.py
@@ -1,11 +1,27 @@
 import pandas as pd
 from pathlib import Path
 #import core.zip_processor as zp
+import unicodedata
+import re
 
 HEADS_DIR = Path("./data/csv/heads")
 ITEMS_DIR = Path("./data/csv/items")
 
 
+
+def normalize_column_name(columns_name):
+    normalize_column = []
+    for cn in columns_name:
+        # Remove acentos, converte texto para minúscula e retirar espaços em branco no inicio e fim do texto
+        cn = unicodedata.normalize("NFKD", cn).encode("ASCII", "ignore").decode().lower().strip()
+        # Substitui todos os não-alfanuméricos por '_'
+        cn = re.sub(r'\W+', '_', cn)
+        # Reduz múltiplos '_' para um só
+        cn = re.sub(r'_+', '_', cn)
+        # Remove '_' do início/fim
+        cn = cn.strip('_')
+        normalize_column.append(cn)
+    return normalize_column
 
 def _read_all_csv_from_dir(directory: Path) -> pd.DataFrame | str:
     """Lê e concatena todos os arquivos CSV de um diretório."""
@@ -13,6 +29,7 @@ def _read_all_csv_from_dir(directory: Path) -> pd.DataFrame | str:
     for csv_file in directory.glob("*.csv"):
         try:
             df = pd.read_csv(csv_file)
+            df.columns = normalize_column_name(df.columns)
             df["__FONTE_ARQUIVO__"] = csv_file.name  # opcional: rastreabilidade
             all_dfs.append(df)
         except Exception as e:
